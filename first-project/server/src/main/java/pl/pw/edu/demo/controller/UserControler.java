98c303129ddaa992069912a3c731c4e55653f411
Linux Duzy <arem1900@gmail.com>
2019-10-27 16:09:03 +0100

    Diffrent approche

--- first-project/server/src/main/java/pl/pw/edu/demo/controller/UserControler.java
+++ first-project/server/src/main/java/pl/pw/edu/demo/controller/UserControler.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin
 @RestController
 @RequestMapping("/api/auth")
 public class UserControler {
